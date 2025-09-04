223cd352cb826925b8777a9bd58654bad2dee7b8
Amao Oluwanifesimi <oluwanifesimiamao@gmail.com>
2025-09-04 20:56:46 +0100

    active screen

---
+++ app/(drawer)/active-reminder/index.tsx
@@ -0,0 +1,11 @@
+import ActiveReminderScreen from "@/screens/active-reminder/active";
+
+const ActiveReminder = () => {
+  return (
+    <>
+      <ActiveReminderScreen />
+    </>
+  );
+};
+
+export default ActiveReminder;
