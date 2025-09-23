5089b6a99f43b887a08d164ba9a42b170859e7cd
Cello-Althub31 <celloalthubengineering@gmail.com>
2025-09-23 15:08:26 +0100

    routing

--- screens/home/home.screen.tsx
+++ screens/home/home.screen.tsx
@@ -66,12 +66,12 @@ export default function HomeScreen() {
       style={{ paddingTop: insets.top }}
       className="flex-1 bg-white"
     >
-      <TopAppBar
+      {/* <TopAppBar
         onMenu={() => console.log("Menu pressed")}
         onNotifications={() => console.log("Notifications")}
         onCall={() => console.log("Call")}
         unreadCount={0}
-      />
+      /> */}
 
       {/* Title + Month */}
       <View className="px-4 mt-6">
